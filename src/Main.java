e1a085b4c8ca0d6b4306a7a8fb163e5ced9209ae
Jinghui Yu <hzbettyyu@gmail.com>
2015-03-10 23:50:31 -0400

    Created first test bot profile and started creating topics for knowledge base

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@
  */
 
 import org.alicebot.ab.*;
+import org.alicebot.ab.utils.IOUtils;
 
 
 public class Main {
@@ -21,12 +22,50 @@ public class Main {
     public static void mainFunction(String[] args) {
         //TODO: load info from a given file and classify the information
 
-        String botName = "mybot";
+        String botName = "currentevents";
         String action = "chat";
         Bot bot = new Bot(botName, MagicStrings.root_path, action); //
 
         boolean doWrites = !action.equals("chat-app");
-        TestAB.testChat(bot, doWrites, MagicBooleans.trace_mode);
+        startChatByBot(bot, doWrites, MagicBooleans.trace_mode);
+    }
+
+    public static void startChatByBot(Bot bot, boolean doWrites, boolean traceMode) {
+        Chat chatSession = new Chat(bot, doWrites);
+        bot.brain.nodeStats();
+        MagicBooleans.trace_mode = traceMode;
+        String textLine = "";
+        String response = chatSession.multisentenceRespond("Human: Hello\n");
+        IOUtils.writeOutputTextLine("Robot", response);
+
+        while(true) {
+            textLine = IOUtils.readInputTextLine("Human");
+            if(textLine == null || textLine.length() < 1) {
+                textLine = MagicStrings.null_input;
+            }
+
+            if(textLine.equals("q")) {
+                System.exit(0);
+            } else if(textLine.equals("wq")) {
+                bot.writeQuit();
+                System.exit(0);
+            } else {
+                if(MagicBooleans.trace_mode) {
+                    System.out.println("STATE=" + textLine + ":THAT=" + ((History)chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
+                }
+
+                response = chatSession.multisentenceRespond(textLine);
+                while (response.contains("&lt;")) {
+                    response = response.replace("&lt;", "<");
+                }
+
+                while(response.contains("&gt;")) {
+                    response = response.replace("&gt;", ">");
+                }
+
+                IOUtils.writeOutputTextLine("Robot", response);
+            }
+        }
     }
 
 
